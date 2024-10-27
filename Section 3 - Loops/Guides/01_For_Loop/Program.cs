554e6b752f6e72b4dcf3005a171bab31ad4b33c3
Dean Coles <dean_coles@outlook.com>
2024-10-27 21:05:39 +0000

    Section 3 Added
    Created folder structure for section 3.

---
+++ Section 3 - Loops/Guides/01_For_Loop/Program.cs
@@ -0,0 +1,22 @@
+﻿// For Loops
+
+// Run code block and increment by 1 until counter is 10
+//
+//      define         condition      increment
+
+for (int counter = 0; counter <= 10; counter++)
+{
+    Console.WriteLine($"Counter is {counter}");
+}
+
+Console.ReadKey();
+
+// Run code block and decrement by 1 until counter is 1
+//
+//      define           condition      decrement
+for (int counter2 = 10; counter2 >= 0; counter2--)
+{
+    Console.WriteLine($"Counter is {counter2}");
+}
+
+Console.ReadKey();
