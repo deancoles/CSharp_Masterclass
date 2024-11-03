9b2604a7409e4f0c64038dc06b3ae5c84b5ffc36
Dean Coles <dean_coles@outlook.com>
2024-11-03 11:34:34 +0000

    Thread.Sleep
    Added Thread.Sleep guide.

---
+++ Section 3 - Loops/Guides/02_Thread_Sleep/Program.cs
@@ -0,0 +1,11 @@
+﻿// Thread.Sleep freezes the program
+
+
+for (int counter = 10; counter >= 0; counter--)
+{
+    Console.WriteLine("Counter is " + counter);
+    Thread.Sleep(1000);
+}
+
+
+Console.ReadKey();
